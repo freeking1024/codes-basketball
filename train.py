796b7fbb1b8c58019941e42b78476196bec83780
wujiahao <freeking1024@163.com>
2025-10-28 23:23:00 +0800

    盖帽上篮train

--- train.py
+++ train.py
@@ -7,7 +7,7 @@ def main():
     # 2. 开始训练
     model.train(
         data="/home/jiahao.wu/DATACENTER1/basketball/ultralytics/ultralytics/cfg/datasets/data.yaml",   # 你的数据集配置文件 (包含 train/val 路径和类别名)
-        epochs=200,         # 训练轮次，可以调整
+        epochs=150,         # 训练轮次，可以调整
         imgsz=640,          # 输入图像大小 (默认640x640)
         batch=16,           # 批量大小，根据你的显存调整
         device=0,           # GPU编号，CPU 就写 "cpu"
